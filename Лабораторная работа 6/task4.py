1dc4f2b0105c81bf0f66fe1199dea75534f70015
Lebedeva-Kate <119015406+Lebedeva-Kate@users.noreply.github.com>
2022-12-24 23:42:21 +0300

    Update task4.py

--- Лабораторная работа 6/task4.py
+++ Лабораторная работа 6/task4.py
@@ -9,10 +9,10 @@ def csv_to_list_dict(file_, delimiter=',') -> list[dict]:
             list_.append(line.rstrip().split(delimiter))
         list_1 = list_[0]
         list_2 = list_[1:]
-        fall_list = []
+        full_list = []
         for i in list_2:
-            fall_list.append(dict(zip(list_1, i)))
-        return fall_list
+            full_list.append(dict(zip(list_1, i)))
+        return full_list
 
 print(json.dumps(csv_to_list_dict(INPUT_FILE), indent=4))
 # перенос строки
